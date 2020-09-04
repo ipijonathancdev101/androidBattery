459266fd1d14334e0f663ac337f3aaf319fdcdd8
LOPEZ GOMEZ Jonathan <john.informatique38@gmail.com>
2020-09-04 10:24:05 +0200

    v1 fonctionnel

---
+++ app/src/main/java/com/example/batteriesystem/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.batteriesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView battery;
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level   = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            battery.setText(String.valueOf(level)+"%");
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        battery = (TextView) this.findViewById(R.id.text1);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+
+
+    }
+
+
+
+}
